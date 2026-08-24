dfd66a32f349e3e395902c8d550346aa4a9db536
naimur <mdnaimurr@gmail.com>
2026-08-24 10:22:13 +0600

    Depenjection Injection create

---
+++ src/app.js
@@ -0,0 +1,25 @@
+/*
+ * Title:
+ * Description:
+ * Author: Md Naimur Rahman
+ * Date: 24/08/2026
+ */
+
+const requestHandler = require("./requestHandler");
+const taskRepository = require("./taskRepository");
+
+const createTaskService = require("./services/taskService");
+
+const createTaskController = require("./controllers/taskController");
+
+const registerRoutes = require("./routes");
+const { addRoute } = require("./router");
+
+const taskService = createTaskService(taskRepository);
+const taskController = createTaskController(taskService);
+
+registerRoutes(addRoute, {
+  taskController,
+});
+
+module.exports = requestHandler;
--- src/services/taskService.js
+++ src/services/taskService.js
@@ -5,48 +5,50 @@
  * Date: 23/08/2026
  */
 
-const taskRepository = require("../taskRepository");
+function createTaskService(taskRepository) {
+  async function createTask(data) {
+    const task = {
+      id: Date.now().toString(),
+      title: data.title.trim(),
+      completed: data.completed ?? false,
+    };
 
-async function createTask(data) {
-  const task = {
-    id: Date.now().toString(),
-    title: data.title.trim(),
-    completed: data.completed ?? false,
+    return taskRepository.create(task);
+  }
+
+  async function getTasks() {
+    return taskRepository.findAll();
+  }
+
+  async function getTaskById(id) {
+    return taskRepository.findById(id);
+  }
+
+  async function updateTask(id, data) {
+    const changes = {};
+
+    if (data.title !== undefined) {
+      changes.title = data.title.trim();
+    }
+
+    if (data.completed !== undefined) {
+      changes.completed = data.completed;
+    }
+
+    return taskRepository.update(id, changes);
+  }
+
+  async function deleteTask(id) {
+    return taskRepository.remove(id);
+  }
+
+  return {
+    createTask,
+    getTaskById,
+    getTasks,
+    updateTask,
+    deleteTask,
   };
-
-  return taskRepository.create(task);
 }
 
-async function getTasks() {
-  return taskRepository.findAll();
-}
-
-async function getTaskById(id) {
-  return taskRepository.findById(id);
-}
-
-async function updateTask(id, data) {
-  const changes = {};
-
-  if (data.title !== undefined) {
-    changes.title = data.title.trim();
-  }
-
-  if (data.completed !== undefined) {
-    changes.completed = data.completed;
-  }
-
-  return taskRepository.update(id, changes);
-}
-
-async function deleteTask(id) {
-  return taskRepository.remove(id);
-}
-
-module.exports = {
-  createTask,
-  getTaskById,
-  getTasks,
-  updateTask,
-  deleteTask,
-};
+module.exports = createTaskService;
